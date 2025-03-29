3686362433368f71865d6db4413a74d84c9e33ae
Vaishnavi2164 <vaishnavivaish2164@gmail.com>
2025-03-29 10:16:32 +0530

    ATE directory commit

--- src/test/java/sample/Sample1.java
+++ src/test/java/sample/Sample1.java
@@ -1,10 +1,13 @@
 package sample;
 
+import org.testng.annotations.Test;
+
 public class Sample1 {
-	
+	@Test
 	public void demo1()
 	{
 		System.out.println("this is demo1");
+		System.out.println("From ATE working directory");
 	}
 
 }
